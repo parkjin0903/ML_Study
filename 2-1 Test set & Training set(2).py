7148c0e4984b03b174acce27bb0a2dfea1163f30
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-04-23 18:09:00 +0900

    Test set & Training set(2)
    numpy
    Shuffle()

--- 2-1 Test set & Training set(2).py
+++ 2-1 Test set & Training set(2).py
@@ -1,3 +1,4 @@
+from operator import index
 from sklearn.neighbors import KNeighborsClassifier
 
 fish_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0, 
@@ -29,4 +30,32 @@ fish_target = [1]*35 + [0]*14
 input_arr = np.array(fish_data)
 target_arr = np.array(fish_target)
 
-print(input_arr)
+'''print(input_arr)
+print(input_arr.shape) #(샘플 수, 특성 수) 출력'''
+
+np.random.seed(42) #seed 값 설정시 random 값이 동일하게 나옴
+index = np.arange(49) #0부터 N-1 까지 증가하는 배열 생성
+np.random.shuffle(index)
+
+train_input = input_arr[index[:35]]
+train_target = target_arr[index[:35]]
+
+test_input = input_arr[index[35:]]
+test_target = target_arr[index[35:]]
+
+print(input_arr[13], train_input[0])
+
+'''print(index)'''
+
+import matplotlib.pyplot as plt
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(test_input[:,0], test_input[:,1])
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+kn = kn.fit(train_input, train_target) #fit() 메서드를 실행할 때 마다 KNeighborsClassifier 클래스의 객체는 이전에 학습한 모든 것을 잃어버림
+kn.score(test_input, test_target)
+kn.predict(test_input)
+
+'''test_target'''
